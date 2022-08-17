083d196a9e751923b739488243366d4a8d124137
Sarwesh Reddy <sarwesh.boje@mutualmobile.com>
2022-08-17 11:20:46 +0530

    Initial Commit

---
+++ PackageUIExample/PackageUIExample.docc/Resources/ExampleView.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleView.swift
+//  
+//
+//  Created by Sarwesh Boje on 17/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+    }
+}
---
+++ PackageUIExample/PackageUIExample.h
@@ -0,0 +1,18 @@
+//
+//  PackageUIExample.h
+//  PackageUIExample
+//
+//  Created by Sarwesh Boje on 17/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PackageUIExample.
+FOUNDATION_EXPORT double PackageUIExampleVersionNumber;
+
+//! Project version string for PackageUIExample.
+FOUNDATION_EXPORT const unsigned char PackageUIExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PackageUIExample/PublicHeader.h>
+
+
